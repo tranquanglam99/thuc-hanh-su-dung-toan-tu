db6ddc005d635fc3e230210c9241dbc4fcad6a3c
tran quang lam <tranquanglamtp@gmail.com>
2021-03-12 02:26:58 +0900

    tạo js

---
+++ area.js
@@ -0,0 +1,6 @@
+let width = prompt(' Nhập chiều rộng !');
+let height = prompt(' Nhập chiều dài !');
+width = parseInt(width);
+height = parseInt(height);
+let area = width*height;
+document.write('Diện tích hình chữ nhật là: ' + area);
